03434c70f9a536f5bc3a559698d0c4d171c5b9bc
niuzhe <nz0811@qq.com>
2022-09-18 20:05:54 +0800

    123456222

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+echo "开始打包"
+git checkout master
+git pull
+
+declare server=$1
+declare port=$2
+mvn clean package -Dmaven.test.skip=true
+
+
+declare targetJar=`ls ./${server}/target/*.jar | grep -v "sources"`
+echo "Get File Sucess:$targetJar"
+
+cp -rf $targetJar ROOT.jar
+
+echo "准备下线服务${server}"
+docker stop ${server}
+docker rm ${server}
+docker rmi ${server}:1.0
+echo "${server}已下线"
+
+echo 开始构建镜像${server}
+docker build -t ${server}:1.0 .
+echo 镜像构建完成${server}
+
+echo 开始运行容器${server}
+docker run -d -p ${port}:${port} --name ${server} ${server}:1.0
+echo test容器运行成功
